f57f636660505dddc9ea823a67cf8cae8a4acffd
dengcc <dengchengchao@outlook.com>
2022-03-31 23:31:08 +0800

    test: add muutli test

--- pool_test.go
+++ pool_test.go
@@ -53,7 +53,7 @@ func TestGetConn(t *testing.T) {
 		t.Errorf("total conn nums must equals %d ,but now %d", IdleSize, p.ConnNum())
 	}
 
-	//wait to conn
+	//wait to connection to server
 	time.Sleep(1 * time.Second)
 	if p.IdleNum() != IdleSize {
 		t.Errorf("total idle conn nums must equals %d ,but now %d", IdleSize, p.IdleNum())
@@ -93,4 +93,44 @@ func TestGetConn(t *testing.T) {
 		t.Errorf("after idle time out ,pool must new at least %d conns,but now %d", IdleSize, atomic.LoadInt32(&newConnNum)-newIdleNum)
 	}
 
+}
+
+func TestMultiGet(t *testing.T) {
+	p := newPooler()
+	if p.ConnNum() != IdleSize {
+		t.Errorf("conn nums must equals %d, but now %d", IdleSize, p.IdleNum())
+	}
+
+	//wait to connection to server
+	time.Sleep(1 * time.Second)
+
+	blockChan := make(chan struct{})
+	for i := 0; i < PoolSize; i++ {
+		go func() {
+			conn, _ := p.Get(context.Background())
+			<-blockChan
+			p.Put(conn, nil)
+		}()
+	}
+
+	done := make(chan struct{}, 1)
+	go func() {
+		cn, _ := p.Get(context.Background())
+		close(done)
+		p.Put(cn, nil)
+	}()
+
+	select {
+	default:
+	case <-done:
+		t.Errorf("pool Get() must block")
+	}
+
+	close(blockChan)
+	time.Sleep(100 * time.Millisecond)
+	select {
+	default:
+		t.Errorf("pool must not block")
+	case <-done:
+	}
 }
